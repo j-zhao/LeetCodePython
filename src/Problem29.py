d626123b6757a4d51d30c3b130783ed4d4d4ee27
Zhao,Jesse <jzhao@vertafore.com>
2016-12-16 10:28:10 -0500

    Shit attempt at trying to solve problem 29

--- src/Problem29.py
+++ src/Problem29.py
@@ -12,7 +12,23 @@ class Problem29(object):
         """
         try:
             quotient = 0
-            
+            while quotient < sys.maxint:
+                sign = 1
+                if dividend < 0:
+                    sign *= -1
+                    dividend *= -1
+                if divisor < 0:
+                    sign *= -1
+                    divisor *= -1
+                if divisor == 1:
+                    return dividend * sign
+                while dividend > 0 and dividend >= divisor:
+                    dividend -= divisor
+                    quotient += 1
+                print(sys.maxint)
+                print(quotient)
+                return quotient * sign
+            return sys.maxint
         except OverflowError as e:
-            print(e)
+            # print(e)
             return sys.maxint
